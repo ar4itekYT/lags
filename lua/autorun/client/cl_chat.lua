8a95f79e72495e958b236ba9562e1d1254031793
ar4itek <ar4itekmail@gmail.com>
2021-12-20 08:10:02 +0700

    1/2 commit (add sendnotify())
    net.Receive("lags_sendnotify", sendnotify)

--- lua/autorun/client/cl_chat.lua
+++ lua/autorun/client/cl_chat.lua
@@ -3,4 +3,11 @@ function sendmsg()
 	Msg("[Защита]: " .. str .. "\n" )
 end
 
+function sendnotify()
+	local str = net.ReadString()
+	notification.AddLegacy( str, NOTIFY_ERROR, 3 )
+	surface.PlaySound( "buttons/button16.wav" )
+end
+
 net.Receive("lags_sendmsg", sendmsg)
+net.Receive("lags_sendnotify", sendnotify)
